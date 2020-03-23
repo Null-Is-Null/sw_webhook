74ea54bbb72950e5e692cfac2722123d2cf11e61
Chris Clement <clemenc@upenn.edu>
2020-03-23 17:11:24 -0400

    Added count field and unique index on name to tag model

--- db/migrate/20200323155319_create_tags.rb
+++ db/migrate/20200323155319_create_tags.rb
@@ -2,8 +2,8 @@ class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
       t.string :name
-
-      t.timestamps
+      t.integer :count, default: 0
     end
+    add_index :tags, :name, unique: true
   end
 end
