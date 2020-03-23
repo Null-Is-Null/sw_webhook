4f434b35ba171bd9663668d677351a0c6c3251ec
Chris Clement <clemenc@upenn.edu>
2020-03-23 18:00:00 -0400

    Added default values for tag:name and ticket:title

--- db/migrate/20200323155308_create_tickets.rb
+++ db/migrate/20200323155308_create_tickets.rb
@@ -2,7 +2,7 @@ class CreateTickets < ActiveRecord::Migration[6.0]
   def change
     create_table :tickets do |t|
       t.integer :user_id
-      t.string :title
+      t.string :title, default: ''
 
       t.timestamps
     end
--- db/migrate/20200323155319_create_tags.rb
+++ db/migrate/20200323155319_create_tags.rb
@@ -1,7 +1,7 @@
 class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, default: ''
       t.integer :count, default: 0
     end
     add_index :tags, :name, unique: true
