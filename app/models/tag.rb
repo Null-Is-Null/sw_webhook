b84fd1b76c2fdbe7ca88e91cca55738610b01a06
Chris Clement <clemenc@upenn.edu>
2020-03-23 17:13:33 -0400

    Added model validations and event handlers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,12 @@
 class Tag < ApplicationRecord
+  has_and_belongs_to_many :tickets
   validates :name, presence: true
 
   before_validation do
     self.name.downcase!
+  end
+
+  before_save do
+    self.increment(:count)
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,12 @@
 class Ticket < ApplicationRecord
-  validates :user_id, presence: true
+  has_and_belongs_to_many :tags
+  validates :user_id, numericality: { only_integer: true }
   validates :title, presence: true
+  validates :tags, length: { maximum: 5 }
+
+  #before_create do
+    #self.tags.each do |tag|
+      #Tag.find_by(name: tag.name)&.increment!(:count)
+    #end
+  #end
 end
