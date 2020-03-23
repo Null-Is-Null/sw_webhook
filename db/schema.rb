2291dc2b85f7d0fa522d20ab9f6888b8eaf7ba83
Chris Clement <clemenc@upenn.edu>
2020-03-23 17:11:58 -0400

    schema.rb updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_155319) do
+ActiveRecord::Schema.define(version: 2020_03_23_164601) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "tags_tickets", id: false, force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "ticket_id", null: false
+    t.index ["ticket_id", "tag_id"], name: "index_tags_tickets_on_ticket_id_and_tag_id", unique: true
   end
 
   create_table "tickets", force: :cascade do |t|
