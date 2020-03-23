f41e6b475b57821fbd9e5e5a7555c10d7924c423
Chris Clement <clemenc@upenn.edu>
2020-03-23 18:39:39 -0400

    Rewrote model unit tests to use factories

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   it "is valid with a name" do
-    expect(Tag.new(name: "Test")).to be_valid
+    expect(build(:tag)).to be_valid
   end
 
   it "has a lowercase name after validation" do
-    tag = Tag.new(name: "Test")
+    tag = build(:tag, name: "Test")
     tag.validate
     expect(tag.name).to eq("test")
   end
 
-  it "is not valid without a name" do
-    expect(Tag.new).to be_invalid
+  it "is not valid with no name" do
+    expect(build(:tag, name: nil)).to be_invalid
   end
 
   it "is not valid with an empty name" do
-    expect(Tag.new(name: '')).to be_invalid
+    expect(build(:tag, name: '')).to be_invalid
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,40 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
   it "is valid with a numeric user ID, title, and no tags" do
-    expect(Ticket.new(user_id: 1, title: "Test")).to be_valid
+    expect(build(:ticket)).to be_valid
   end
 
   it "is valid with a numeric user ID, title, and five tags" do
-    ticket = Ticket.new(user_id: 1, title: "Test")
-    ticket.tags = (1..5).map do |n| Tag.new(name: "tag_#{n}") end
+    ticket = build(:ticket, tags: (1..5).map do |n| Tag.new(name: "tag_#{n}") end)
     expect(ticket).to be_valid
   end
 
   it "is invalid with a non-numeric user ID" do
-    expect(Ticket.new(user_id: "XYZ", title: "Test")).to be_invalid
+    expect(build(:ticket, user_id: 'xyz')).to be_invalid
   end
 
   it "is invalid with no user ID" do
-    expect(Ticket.new(title: "Test")).to be_invalid
+    expect(build(:ticket, user_id: nil)).to be_invalid
   end
 
   it "is invalid with no title" do
-    expect(Ticket.new(user_id: 1)).to be_invalid
+    expect(build(:ticket, title: nil)).to be_invalid
   end
 
   it "is invalid with an empty title" do
-    expect(Ticket.new(user_id: 1, title: '')).to be_invalid
+    expect(build(:ticket, title: '')).to be_invalid
   end
 
   it "is invalid with six tags" do
-    ticket = Ticket.new(user_id: 1, title: "Test")
-    ticket.tags = (1..6).map do |n| Tag.new(name: "tag_#{n}") end
+    ticket = build(:ticket, tags: (1..6).map do |n| Tag.new(name: "tag_#{n}") end)
     expect(ticket).to be_invalid
   end
 
   it "is invalid with an invalid tag" do
-    ticket = Ticket.new(user_id: 1, title: "Test")
-    ticket.tags = [Tag.new]
+    ticket = build(:ticket, tags: [Tag.new])
     expect(ticket).to be_invalid
   end
 end
